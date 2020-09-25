30319613f6f0450a9e392cc71470ce9ff4ce0e43
Nicky <362330721@qq.com>
2020-09-25 17:20:20 +0800

    Create jarRun.sh

---
+++ jarRun.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+JAVA_PATH=/u01/jdk1.8.0_11/bin
+jps -l | grep api-process-info-service.jar | xargs echo 'kill -9'
+jps -l | grep api-process-info-service.jar | awk  '{print $1}'  | xargs kill -9
+sleep 5
+nohup ${JAVA_PATH}/java -jar  -Xms512m -Xmx512m -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=7074 -Dserver.port=8003 appr-process-info-service.jar >8003.logs &
+tail -f 8001.logs
